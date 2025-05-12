705b9607d34093e7945290a66b18998a63e709cc
Sellaris <3378709702@qq.com>
2025-05-12 20:01:12 +0800

    DEBUG

--- tempCodeRunnerFile.py
+++
@@ -1,3 +0,0 @@
-        print(f"Loss: {(sum(train_loss['loss_ab']) + sum(train_loss['loss_self'])) / (n_batches_ab + n_batches_self):.5f}")
-        return train_loss, {
-            "p_loss": (sum(train_loss["p_loss_ab"]) + sum(train_loss["p_loss_self"])) / (n_batches_ab + n_batches_self),
